9ad6fc9d8ceaa37e7c80365960a3d856428b0759
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-11-29 16:06:52 +0530

    started working on js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,21 @@
-console.log('This is good');
+function calculateTip(billAmount,tipRate,numOfPeople = 1) {
+  const tipPerPerson =( billAmount *  (tipRate / 100) ) / numOfPeople;
+  const totalPerPerson = (billAmount + (billAmount * (tipRate / 100))) / numOfPeople;
+  console.log(tipPerPerson, totalPerPerson);
+
+  return { tipPerPerson, totalPerPerson };
+}
+
+const tip = document.querySelector('.calculated-tip');
+const total = document.querySelector('.calculated-total');
+
+function displayCalculation(x) {
+  const tipPerPerson = x.tipPerPerson;
+  const totalPerPerson = x.totalPerPerson;
+  console.log(tipPerPerson,totalPerPerson);
+  tip.textContent =`$${tipPerPerson.toFixed(2)}` ;
+  total.textContent = `$${totalPerPerson.toFixed(2)}`;
+}
+
+const a = calculateTip(500,20,2);
+displayCalculation(a);
