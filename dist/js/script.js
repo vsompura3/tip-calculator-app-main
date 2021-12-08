9659483227dac7eb1a544d9c97bd266ad9757cc7
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-12-08 23:55:28 +0530

    Started from scratch

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,25 @@
-function calculateTip(billAmount,tipRate,numOfPeople = 1) {
-  const tipPerPerson =( billAmount *  (tipRate / 100) ) / numOfPeople;
-  const totalPerPerson = (billAmount + (billAmount * (tipRate / 100))) / numOfPeople;
-  console.log(tipPerPerson, totalPerPerson);
+// function calculateTip(billAmount,tipRate,numOfPeople = 1) {
+//   const tipPerPerson =( billAmount *  (tipRate / 100) ) / numOfPeople;
+//   const totalPerPerson = (billAmount + (billAmount * (tipRate / 100))) / numOfPeople;
+//   console.log(tipPerPerson, totalPerPerson);
 
-  return { tipPerPerson, totalPerPerson };
-}
+//   return { tipPerPerson, totalPerPerson };
+// }
 
-const tip = document.querySelector('.calculated-tip');
-const total = document.querySelector('.calculated-total');
+// const tip = document.querySelector('.calculated-tip');
+// const total = document.querySelector('.calculated-total');
 
-function displayCalculation(x) {
-  const tipPerPerson = x.tipPerPerson;
-  const totalPerPerson = x.totalPerPerson;
-  console.log(tipPerPerson,totalPerPerson);
-  tip.textContent =`$${tipPerPerson.toFixed(2)}` ;
-  total.textContent = `$${totalPerPerson.toFixed(2)}`;
-}
+// function displayCalculation(x) {
+//   const tipPerPerson = x.tipPerPerson;
+//   const totalPerPerson = x.totalPerPerson;
+//   console.log(tipPerPerson,totalPerPerson);
+//   tip.textContent =`$${tipPerPerson.toFixed(2)}` ;
+//   total.textContent = `$${totalPerPerson.toFixed(2)}`;
+// }
 
-const a = calculateTip(500,20,2);
-displayCalculation(a);
+// const a = calculateTip(500,20,2);
+// displayCalculation(a);
+
+/* 
+=
+*/
