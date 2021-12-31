4bb99fcd039bd30d85632aac24b6e2f196e46e56
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-12-31 17:38:56 +0530

    Redid the whole app

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,73 +1,64 @@
-// Selet elements of the page
-const billInput = document.getElementById('bill-input');
-const peopleInput = document.getElementById('people-input');
-const customInput = document.getElementById('custom-input');
-const tipOptions = Array.from(document.querySelectorAll(`input[type='radio']`));
-const defaultInputTip = tipOptions.find(tip => tip.checked ? tip : null);
-const defaultTip = Number(defaultInputTip.value);
-const tip = document.querySelector('.result__tip-per-person');
-const total = document.querySelector('.result__total-per-person');
-const warn = document.querySelector('.warning');
+const billInput = document.getElementById('bill-input')
+const peopleInput = document.getElementById('people-input')
+const customTip = document.getElementById('custom-tip')
+const tipOptions = Array.from(document.querySelectorAll('input[type="radio"]'))
+const tip = document.getElementById('display-tip')
+const total = document.getElementById('display-total')
+const btnReset = document.querySelector('button[type="reset"]')
 
-// Variables to hold values
-let billAmount, numOfPeople, tipRate;
+let amount
+let numOfPeople
+let tipPercentage
+let customTipPercantage
 
-// Function that checks validation of input
-function validateInput(input) {
-  const inputValue = Number(input.value);
-  if (inputValue > 0) {
-    warn.classList.add('hide');
-    return inputValue;
-  } else{
-    warn.classList.remove('hide');
-    inputValue = 0;
-    input.textContent = '0';
-  }
-}
+billInput.addEventListener('input', handleEvent)
+peopleInput.addEventListener('input', handleEvent)
+customTip.addEventListener('input', handleEvent)
+tipOptions.forEach(opt => {
+  opt.addEventListener('change', handleEvent)
+})
 
-// Calculate the Tip and return tipPerPerson, totalPerPerson as an object
-function calculation(billAmount, tipRate, numOfPeople = 1){
-  const tipPerPerson =( billAmount *  (tipRate / 100) ) / numOfPeople;
-  const totalPerPerson = (billAmount + (billAmount * (tipRate / 100))) / numOfPeople;
-  console.log(tipPerPerson, totalPerPerson);
-  return { tipPerPerson, totalPerPerson };
-}
-
-// Function that takes an object and puts in on the DOM
-function updateUI({tipPerPerson,totalPerPerson}) {
-  tip.textContent = '$'+tipPerPerson.toFixed(2);
-  total.textContent = '$'+totalPerPerson.toFixed(2);
-}
-
-// Function that handles input events
 function handleEvent(e) {
-  if (e.target.id === 'bill-input') {
-    billAmount = validateInput(e.target);
-  }
-  if (e.target.id === 'people-input') {
-    numOfPeople = validateInput(e.target);
-  } else{
-    numOfPeople = 1;
-  }
-  if (e.target.id === 'custom-input'){
-    tipRate = Number(e.target.value);
-    defaultInputTip.checked = false;
-  } else if(e.target.checked) {
-    tipRate = Number(e.target.value);
-  } else{
-    tipRate = defaultTip;
-  }
+  amount = +billInput.value
+  numOfPeople =
+    e.target.id === 'people-input' && e.target.value > 0 ? +e.target.value : 1
 
+  tipOptions.forEach(opt => {
+    if (e.target.id === 'custom-tip') {
+      tipPercentage = +customTip.value
+      opt.checked = false
+    }
+    if (opt.checked) {
+      customTip.value = ''
+      tipPercentage = +opt.value
+    }
+  })
 
-  const output = calculation(billAmount, tipRate, numOfPeople);
-  updateUI(output)
+  console.log(amount, numOfPeople, tipPercentage)
+  const result = calculate(amount, tipPercentage, numOfPeople)
+  displayResult(result)
 }
 
+function calculate(billAmount, tipRate, numofPeople = 1) {
+  const tipPerPerson = (billAmount * (tipRate / 100)) / numOfPeople
+  const totalPerPerson =
+    (billAmount + billAmount * (tipRate / 100)) / numOfPeople
+  return {
+    tipPerPerson,
+    totalPerPerson,
+  }
+}
 
-// Setting Event handlers
-billInput.addEventListener('input', handleEvent);
-peopleInput.addEventListener('input',handleEvent);
-customInput.addEventListener('input', handleEvent)
-tipOptions.forEach(t => {
-  t.addEventListener('input', handleEvent);
+function displayResult({tipPerPerson, totalPerPerson}) {
+  tip.textContent = `$${tipPerPerson.toFixed(2)}`
+  total.textContent = `$${totalPerPerson.toFixed(2)}`
+}
+
+btnReset.addEventListener('click', () => {
+  billInput.value = ''
+  peopleInput.value = ''
+  customTip.value = ''
+  document.getElementById('15').checked = true
+  tip.textContent = '$0.00'
+  total.textContent = '$0.00'
 })
